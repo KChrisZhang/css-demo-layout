5dfdfb8813bf767d418d30270b62728af2dd4a6c
kchris <3430567455@qq.com>
2018-04-20 16:26:26 +0800

    Fluid layout: fixed width and query media.

---
+++ src/components/HelloResponsive.vue
@@ -0,0 +1,104 @@
+<template>
+  <div id="hello-responsive">
+    <aside class="fixed-aside">
+      <ul>
+        <li class="list-item" v-for="item in list" :key="item.index"></li>
+      </ul>
+    </aside>
+    <section class="main"></section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloResponsive',
+  data () {
+    return {
+      msg: '',
+      list: [1, 2, 3, 4, 5]
+    }
+  },
+  mounted () {
+    console.log('HelloResponsive [Component]')
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// 四种适配类型：手机、平板、笔记本、台式电脑
+#hello-responsive {
+  display: flex;
+
+  width: 80%;
+  height: 1500px;
+  margin: 0 auto;
+  background-color: #fff;
+
+  .fixed-aside {
+    width: 300px;
+    height: 100%;
+    margin-right: 20px;
+    background-color: orange;
+    overflow: hidden;
+  }
+
+  .main {
+    flex: 1;
+    height: 100%;
+    background-color: pink;
+    overflow: hidden;
+  }
+
+  .list-item {
+    width: 100%;
+    height: 300px;
+    margin-bottom: 20px;
+    background-color: red;
+  }
+}
+
+@media (min-width: 1366px) {
+  #hello-responsive {
+    width: 1200px;
+  }
+}
+
+@media (max-width: 1366px) {
+  #hello-responsive {
+
+  }
+}
+
+@media (max-width: 800px) {
+  #hello-responsive {
+    width: calc(100% - 40px * 2);
+    margin: 0 40px;
+
+    .fixed-aside {
+      width: 200px;
+    }
+
+    .list-item {
+      height: 200px;
+    }
+  }
+}
+
+@media (max-width: 450px) {
+  #hello-responsive {
+    width: calc(100% - 20px * 2);
+    margin: 0 20px;
+
+    .fixed-aside {
+      width: 100px;
+      margin-right: 10px;
+    }
+
+    .list-item {
+      height: 100px;
+      margin-bottom: 10px;
+    }
+  }
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,21 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import HelloLayout from '@/components/HelloLayout'
+import HelloResponsive from '@/components/HelloResponsive'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/layout',
       name: 'HelloLayout',
       component: HelloLayout
+    },
+    {
+      path: '/',
+      name: 'HelloResponsive',
+      component: HelloResponsive
     },
     {
       path: '/hello',
